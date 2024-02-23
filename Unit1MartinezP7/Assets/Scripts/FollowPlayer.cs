649379bbba472ea70a4cef3e92e6f4ba6ec00017
Jayden Martinez <724583@student.dusd.net>
2024-02-23 15:26:27 -0800

    Car go weeeee
    added obstacles, car moves, can control it, camera follows it too

---
+++ Unit1MartinezP7/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class FollowPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -7);
+    
+
+
+    //Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
