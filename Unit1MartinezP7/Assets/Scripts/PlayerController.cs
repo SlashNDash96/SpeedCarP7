abd66b3e2008ad4c2b40932231d8481f979a6723
Jayden Martinez <724583@student.dusd.net>
2024-03-04 15:27:35 -0800

    RACE CARS LASER AIIIIRPLANES
    Finished car part, tryna fix a plane now

--- Unit1MartinezP7/Assets/Scripts/PlayerController.cs
+++ Unit1MartinezP7/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
+    public string inputID;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +22,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        horizontalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal" + inputID);
+        horizontalInput = Input.GetAxis("Vertical" + inputID);
         // Moves the car forward based on vertical input
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
         // Rotates the car based on horizontal input
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        if (Input.GetKey(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !mainCamera.enabled;
+        }
     }
 }
